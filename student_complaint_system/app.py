4daf8bffc912356c01844e82db77c2460d1aa13d
Tamali2005 <tuli.khan1433@gmail.com>
2026-02-28 00:40:36 +0530

    z

--- student_complaint_system/app.py
+++ student_complaint_system/app.py
@@ -9,7 +9,6 @@ from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
 import secrets
 from werkzeug.utils import secure_filename
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
@@ -25,14 +24,7 @@ if not os.path.exists(UPLOAD_FOLDER):
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f"mysql+pymysql://{os.getenv('MYSQLUSER')}:"
-    f"{os.getenv('MYSQLPASSWORD')}@"
-    f"{os.getenv('MYSQLHOST')}:"
-    f"{os.getenv('MYSQLPORT')}/"
-    f"{os.getenv('MYSQLDATABASE')}"
-)
-db = SQLAlchemy(app)
+
 
 # Google OAuth Configuration
 GOOGLE_CLIENT_ID = "544206990021-o4ve57tle2tu6ag8s47dta87r547q4ir.apps.googleusercontent.com"  # Replace with your actual Client ID
@@ -52,7 +44,7 @@ DB_CONFIG = {
     'database': os.getenv("MYSQLDATABASE"),
     'port': int(os.getenv("MYSQLPORT"))
 }
-conn = mysql.connector.connect(**DB_CONFIG)
+
 
 # Initialize Database
 def init_db():
